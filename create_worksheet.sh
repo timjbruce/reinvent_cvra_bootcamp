8a77a03ce60a4dd5ac8eecd8a55afba7294c9a7c
dixonaws@amazon.com <dixonaws@amazon.com>
2018-10-24 10:41:17 -0400

    updated instructions

--- create_worksheet.sh
+++ create_worksheet.sh
@@ -4,7 +4,6 @@ echo "reInvent CVRA Bootcamp Worksheet" > worksheet.txt
 echo "Work directory: " `pwd` >> worksheet.txt
 echo "OS: " `uname -a` >> worksheet.txt
 echo "Git: " `git --version` >> worksheet.txt
-echo "AWS CLI: " `aws --version` >> worksheet.txt
 echo "Python: " `python -c "import sys;print(sys.version)"` >> worksheet.txt
 echo "ASK CLI: " `ask --version` >> worksheet.txt
 echo "Virtualenv:" `virtualenv --version` >> worksheet.txt
